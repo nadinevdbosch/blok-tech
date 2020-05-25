ec0a26f308c1ef2e86d2e560c13488e7ff5e61a0
nadinevdbosch <nadinevandenbosch@gmail.com>
2020-05-25 20:20:46 +0200

    add templating

---
+++ backend/templating-ejs/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+express()
+    .set('view engine', 'ejs')
+    .use(express.static('static'))
+    .get('/', home)
+    .get('/test', test)
+    .get('/hoi', hoi)
+    .get('/profile/:name', profile)
+    .listen(3000)
+
+function home(req, res) {
+    res.status(200).send('Dit is de homepagina');
+}
+ 
+function test(req, res) {
+    res.status(200).send('Dit is een testpagina');
+}
+
+function hoi(req, res) {
+    res.status(200).send('Hoi. Wat leuk dat je hier een kijkje  neemt');
+}
+
+function profile(req, res) {
+    res.render('profile', {name: req.params.name})
+}
+
