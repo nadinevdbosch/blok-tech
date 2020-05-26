885b0bb30a333326ce94b0706401a7bb1123da0a
nadinevdbosch <nadinevandenbosch@gmail.com>
2020-05-26 21:55:42 +0200

    updating templating-handlebars

--- backend/templating-handlebars/index.js
+++ backend/templating-handlebars/index.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const app = express();
 const expressHandlebars = require('express-handlebars');
+const path = require('path');
 
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main', 
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+// app.set('views', 'views');
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 // app.locals.layout = false;
 
 //routing
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/home', (req, res) => {
+    res.render('index', { 
+        title: 'Home'
+    });
 });
 
 app.get('/about', function(req, res){
-    res.render('about');
+    res.render('about', { 
+        title: 'About',
+        name: 'Nadine',
+        displayName: false
+     });
+});
+
+app.get('/lists', function(req, res){
+    res.render('lists', { 
+        title: 'lists',
+        list: [
+            {
+                items: ['banaan', 'appel', 'peer']
+            },
+
+            {
+                items: ['melk', 'sap', 'cola']
+            }
+        ]
+     });
 });
 
 app.listen(8000);
