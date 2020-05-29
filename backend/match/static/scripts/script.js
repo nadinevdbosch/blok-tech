86a7a4ac91d41329705564f1096bbbccf1b18dfb
nadinevdbosch <nadinevandenbosch@gmail.com>
2020-05-29 19:44:52 +0200

    add datingapp match

---
+++ backend/match/static/scripts/script.js
@@ -0,0 +1,13 @@
+//script voor hamburgermenu
+var hamburger = document.querySelector('.hamburger');
+var cross = document.querySelector('.cross');
+var navigatie = document.querySelector('nav');
+
+function active() {
+    navigatie.classList.toggle('menu-items-active');
+    hamburger.classList.toggle('display-none');
+    cross.classList.toggle('display-none');
+}
+
+hamburger.addEventListener('click', active);
+cross.addEventListener('click', active);
